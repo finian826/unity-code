62313b5ef4f50695793fb9ba79519572fbb9b89e
finian826 <tmacmill@gmail.com>
2015-09-13 00:09:47 -0400

    Nightly Commit
    Nightly dump

---
+++ 2d/BlockBuster/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject OneHitBrick, TwoHitBrick, ThreeHitBrick, SpecialBrick, Invincible;
+	//public ArrayList BlockLevels;
+	public Sprite[] Background;
+	public Vector4[] BlockLocs;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
