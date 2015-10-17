705ee03c673b153918f6ddf6cb0397a5cf74addf
finian826 <tmacmill@gmail.com>
2015-10-17 19:20:23 -0400

    More Admin
    More Admin Changes

--- RPG Daemon/rpgd/rpgd/setup.cpp
+++ RPG Daemon/rpgd/rpgd/setup.cpp
@@ -1,10 +1,32 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "c_tokenizer.h"
+using namespace std;
 
 void DoSetup() 
 {
 	// Table Setup
 
 
+}
+
+void ReadConfig() 
+{
+	string line;
+	const char* token;
+	unsigned    n;
+	
+	ifstream myfile ("config.ini");
+	if (myfile.is_open())
+	{
+		while ( getline (myfile,line) )
+		{
+			tokenizer_t tok = tokenizer( line, ",", TOKENIZER_EMPTIES_OK );
+		}
+		myfile.close();
+	}
 }
